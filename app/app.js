78d3e7b38fb6c42f039cba9324d01751c3f7d6b3
note109 <note109+github@gmail.com>
2016-11-07 18:42:11 +0900

    split circle and stage

--- app/app.js
+++ app/app.js
@@ -1,17 +1,34 @@
 let stage;
 
 $(() => {
-  stage = new Stage();
+  const circle = new Circle(150, 150, 40);
+
+  stage = new Stage([circle]);
 });
 
 $(window).on("resize", () => {
   stage.init();
 });
 
+class Circle {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
+}
+
 class Stage {
-  constructor() {
+  constructor(contents) {
     this.canvas = document.getElementById("stage");
     this.ctx = this.canvas.getContext("2d");
+    this.contents = contents;
 
     this.init();
   }
@@ -28,8 +45,9 @@ class Stage {
   render() {
     this.ctx.clearRect(0, 0, this.width, this.height);
 
-    this.ctx.beginPath();
-    this.ctx.arc(50, 50, 40, 0, Math.PI * 2, true);
-    this.ctx.fill();
+    this.contents.forEach((cnt) => {
+      cnt.render(this.ctx);
+    });
+    requestAnimationFrame(::this.render);
   }
 }
--- dest/app.js
+++ dest/app.js
@@ -7,19 +7,43 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var stage = void 0;
 
 $(function () {
-  stage = new Stage();
+  var circle = new Circle(150, 150, 40);
+
+  stage = new Stage([circle]);
 });
 
 $(window).on("resize", function () {
   stage.init();
 });
 
+var Circle = function () {
+  function Circle(x, y, r) {
+    _classCallCheck(this, Circle);
+
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+
+  _createClass(Circle, [{
+    key: "render",
+    value: function render(ctx) {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+      ctx.fill();
+    }
+  }]);
+
+  return Circle;
+}();
+
 var Stage = function () {
-  function Stage() {
+  function Stage(contents) {
     _classCallCheck(this, Stage);
 
     this.canvas = document.getElementById("stage");
     this.ctx = this.canvas.getContext("2d");
+    this.contents = contents;
 
     this.init();
   }
@@ -37,11 +61,14 @@ var Stage = function () {
   }, {
     key: "render",
     value: function render() {
+      var _this = this;
+
       this.ctx.clearRect(0, 0, this.width, this.height);
 
-      this.ctx.beginPath();
-      this.ctx.arc(50, 50, 40, 0, Math.PI * 2, true);
-      this.ctx.fill();
+      this.contents.forEach(function (cnt) {
+        cnt.render(_this.ctx);
+      });
+      requestAnimationFrame(this.render.bind(this));
     }
   }]);
 
