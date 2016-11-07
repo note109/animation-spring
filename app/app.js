dd553eb38f65527f4fc2240a1974cddb9e93c34f
note109 <note109+github@gmail.com>
2016-11-07 18:22:37 +0900

    :sparkles: create canvas with circle

--- app/app.js
+++ app/app.js
@@ -1,1 +1,35 @@
-console.log("helo")
+let stage;
+
+$(() => {
+  stage = new Stage();
+});
+
+$(window).on("resize", () => {
+  stage.init();
+});
+
+class Stage {
+  constructor() {
+    this.canvas = document.getElementById("stage");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.init();
+  }
+
+  init() {
+    this.width = $(".wrapper").width();
+    this.height = $(".wrapper").height();
+    this.canvas.setAttribute("width", this.width);
+    this.canvas.setAttribute("height", this.height);
+
+    this.render();
+  }
+
+  render() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+
+    this.ctx.beginPath();
+    this.ctx.arc(50, 50, 40, 0, Math.PI * 2, true);
+    this.ctx.fill();
+  }
+}
--- dest/app.js
+++ dest/app.js
@@ -1,3 +1,49 @@
 "use strict";
 
-console.log("helo");
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var stage = void 0;
+
+$(function () {
+  stage = new Stage();
+});
+
+$(window).on("resize", function () {
+  stage.init();
+});
+
+var Stage = function () {
+  function Stage() {
+    _classCallCheck(this, Stage);
+
+    this.canvas = document.getElementById("stage");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.init();
+  }
+
+  _createClass(Stage, [{
+    key: "init",
+    value: function init() {
+      this.width = $(".wrapper").width();
+      this.height = $(".wrapper").height();
+      this.canvas.setAttribute("width", this.width);
+      this.canvas.setAttribute("height", this.height);
+
+      this.render();
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      this.ctx.clearRect(0, 0, this.width, this.height);
+
+      this.ctx.beginPath();
+      this.ctx.arc(50, 50, 40, 0, Math.PI * 2, true);
+      this.ctx.fill();
+    }
+  }]);
+
+  return Stage;
+}();
