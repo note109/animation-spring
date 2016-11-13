a5ef514bbf7631c9835b36179cea7eb30cdca216
note109 <note109+github@gmail.com>
2016-11-13 21:13:28 +0900

    dest

--- dest/app.js
+++ dest/app.js
@@ -47,6 +47,7 @@ var Handle = function () {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.dragged = false;
   }
 
   _createClass(Handle, [{
@@ -66,12 +67,18 @@ var Handle = function () {
     key: "isDragged",
     value: function isDragged() {
       if (cursor.dragging === false) {
-        return false;
+        this.dragged = false;
+
+        return this.dragged;
       }
       var dx = Math.abs(this.x - cursor.x);
       var dy = Math.abs(this.y - cursor.y);
 
-      return dx <= this.r && dy <= this.r;
+      if (dx <= this.r && dy <= this.r) {
+        this.dragged = true;
+      }
+
+      return this.dragged;
     }
   }, {
     key: "handleDrag",
@@ -128,6 +135,13 @@ var Circle = function () {
       ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
       ctx.fillStyle = "rgba(155, 187, 89, 0.8)";
       ctx.fill();
+
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(this.chainTos[0].x, this.chainTos[0].y);
+      ctx.lineWidth = 30;
+      ctx.strokeStyle = "rgba(155, 187, 89, 0.8)";
+      ctx.stroke();
     }
   }, {
     key: "getTargetX",
